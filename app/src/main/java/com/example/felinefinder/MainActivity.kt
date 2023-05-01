67b7092b049f4ac566d8e6948fc1edef51042297
AmandaLi0 <amanda.om.li25@gmail.com>
2023-05-01 09:13:17 -0700

    main activty copied w super call

--- app/src/main/java/com/example/felinefinder/MainActivity.kt
+++ app/src/main/java/com/example/felinefinder/MainActivity.kt
@@ -62,6 +62,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         val permissionsToRequest = ArrayList<String>()
         var i = 0
         while (i < grantResults.size) {
