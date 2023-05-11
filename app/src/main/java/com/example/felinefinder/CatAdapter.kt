848436a29219bab3afc189552c7975e0b08c5b59
ellen <ellenleung4@gmail.com>
2023-05-11 09:01:50 -0700

    adapter fixes

--- app/src/main/java/com/example/felinefinder/CatAdapter.kt
+++ app/src/main/java/com/example/felinefinder/CatAdapter.kt
@@ -4,11 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class CatAdapter : AppCompatActivity() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textViewName: TextView
+        val textViewDesc : TextView
+        val textViewFriendly : TextView
+        val layout : ConstraintLayout
+
+        init {
+            // Define click listener for the ViewHolder's View
+            textViewName = view.findViewById(R.id.textView_catitem_name)
+            textViewDesc = view.findViewById(R.id.textView_catitem_descr)
+            textViewFriendly = view.findViewById(R.id.textView_catitem_friendly)
+            layout = view.findViewById(R.id.layout_catItem)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cat_item)
@@ -18,10 +35,10 @@ class CatAdapter : AppCompatActivity() {
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.activity_main, viewGroup, false)
 
-        return RecyclerView.ViewHolder(view) //weeee
+        return ViewHolder(view) //weeee
     }
 
-    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
