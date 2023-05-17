e1af2002902ea88a2b4b54193546f79511e917cd
AmandaLi0 <amanda.om.li25@gmail.com>
2023-05-17 10:07:38 -0700

    yes

--- app/src/main/java/com/example/felinefinder/CatAddInput.kt
+++ app/src/main/java/com/example/felinefinder/CatAddInput.kt
@@ -5,31 +5,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.felinefinder.databinding.ActivityCatAddInputBinding
 
 class CatAddInput : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_cat_add_input)
+//    }
+    private lateinit var binding: ActivityCatAddInputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cat_add_input)
-    }
-    private lateinit var binding: ActivityRegistrationBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        binding = ActivityCatAddInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME) ?: ""
-        val password = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD) ?: ""
+//        val username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME) ?: ""
+//        val password = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD) ?: ""
 
-        binding.editTextRegistrationUsername.setText(username)
-        binding.editTextRegistrationPassword.setText(password)
+//        binding.editTextRegistrationUsername.setText(username)
+//        binding.editTextRegistrationPassword.setText(password)
 
         //register an account and send back the username and password to the login activity to prefill those fields
-        binding.buttonRegistrationRegister.setOnClickListener {
-            val name = binding.editTextRegistrationName.text.toString()
-            val breed = binding.editTextRegistrationConfirmPassword.text.toString()
-            val friendly = binding.editTextRegistrationUsername.text.toString()
-            val lat = binding.editTextRegistrationName.text.toString()
-            val long = binding.editTextTextEmailAddress.text.toString()
+        binding.buttonAddRegister.setOnClickListener {
+            val name = binding.editTextAddName.text.toString()
+            val breed = binding.editTextAddBreed.text.toString()
+            val friendly = binding.editTextAddFriendly.text.toString()
+            val lat = binding.editTextAddLat.text.toString()
+            val long = binding.editTextAddLong.text.toString()
 
             MapFragment.addIcon(Double: lat, Double :long)
             //weeee
